4c1d3ce760ea3025c8255436ee1102c3c704b7c9
SamsonMokaya <mokayasamson950@gmail.com>
2023-07-15 14:40:11 +0300

    Setting up

---
+++ routes/ridesRoute.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+
+router.route("/").get((req, res)=>{
+    res.status(200).json({
+        message: "Get all rides"
+    })
+})
+
+
+module.exports = router;
